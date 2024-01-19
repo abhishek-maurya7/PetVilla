f3d425aebd4744919b083d27fa29cfed9b09b9a2
Abhishek Maurya <mauryaabhishek279@gmail.com>
2024-01-19 17:34:39 +0530

    Initialized Project

---
+++ src/main/java/com/jsd/petvilla/PetvillaApplication.java
@@ -0,0 +1,13 @@
+package com.jsd.petvilla;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetvillaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetvillaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jsd/petvilla/PetvillaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jsd.petvilla;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetvillaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
